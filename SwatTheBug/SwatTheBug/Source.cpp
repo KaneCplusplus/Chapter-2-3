4dac09a91893c069496e47ce39519689ed53009c
Kane-Greenwalt <kanegreenwalt3@gmail.com>
2018-04-05 12:41:59 -0500

    SwatTheBugExercise

---
+++ SwatTheBug/SwatTheBug/Source.cpp
@@ -0,0 +1,21 @@
+//SwatTheBugs16.cpp
+//Created/revised by Kane Greenwalt on 04/05/18
+
+#include <iostream>
+
+
+int main()
+{
+	//declare variable
+
+	//increase variable's value by 1.5
+	double temp = 35.3;
+	double tempAfterAdd = temp + 1.5;
+	
+
+		//display variable's value
+		std::cout << "After adding 1.5, the temp variable now contains " << tempAfterAdd << "." << std::endl;
+
+	system("pause");
+	return 0;
+}	
