d583e0d8db7b1090c768ecbe9f683fcc63eef7c8
Kane-Greenwalt <kanegreenwalt3@gmail.com>
2018-04-06 12:35:46 -0500

    Eggs! Finshed

--- Eggs!/Eggs!/Source.cpp
+++ Eggs!/Eggs!/Source.cpp
@@ -6,8 +6,24 @@
 
 int main()
 {
+	int DozenEggs = 12;
+	int SingleEggsEntered;
+	int dozenEggsCarton;
+	int eggsRemainder;
+
+	std::cout << "How many eggs do you want?" << std::endl;
+	std::cin >> SingleEggsEntered;
+	
+	dozenEggsCarton = SingleEggsEntered / DozenEggs;
+	eggsRemainder = SingleEggsEntered % DozenEggs;
+
+	std::cout << "You have " << dozenEggsCarton << " dozen and " << eggsRemainder << std::endl;
+
+	double total = eggsRemainder * .25 + dozenEggsCarton * 2.00;
+	std::cout << "Total is $" << total;
 
 
 
-
+	system("Pause");
+	return 0;
 }
